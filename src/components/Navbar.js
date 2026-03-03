3e67a4d31fedb341884455ee47f47369b33d89d4
KodeeswaranKani <kodeeswaran4824@gmail.com>
2026-03-03 20:46:36 +0530

    Navbar closing at mobile view after click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
 import { Link } from "react-router-dom";
 // import { FaChevronDown } from "react-icons/fa"; 
 import RG_Logo from "./../assets/RG_logo.jpeg";
+import Collapse from "bootstrap/js/dist/collapse";
 
 function Navbar() {
+
+  const closeNavbar = () => { 
+        const navbar = document.getElementById("nav");
+      if (navbar) {
+        const bsCollapse = Collapse.getInstance(navbar) 
+          || new Collapse(navbar);
+        bsCollapse.hide();
+      }
+  };
+
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light sticky-top">
       <div className="container">
@@ -16,22 +28,22 @@ function Navbar() {
 
         <div className="collapse navbar-collapse" id="nav">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/about">About Us</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/" onClick={closeNavbar}>Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/about" onClick={closeNavbar}>About Us</Link></li>
 
             <li className="nav-item dropdown">
               <Link className="nav-link dropdown-toggle" to="/services" data-bs-toggle="dropdown">
                 Services  
               </Link>
               <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="/image-processing">Image Processing</Link></li>
-                <li><Link className="dropdown-item" to="/document-digitalization">Document Digitalization</Link></li>
-                <li><Link className="dropdown-item" to="/back-office">Back Office Services</Link></li>
-                <li><Link className="dropdown-item" to="/data-processing">Data Processing</Link></li>
+                <li><Link className="dropdown-item" to="/image-processing" onClick={closeNavbar}>Image Processing</Link></li>
+                <li><Link className="dropdown-item" to="/document-digitalization" onClick={closeNavbar}>Document Digitalization</Link></li>
+                <li><Link className="dropdown-item" to="/back-office" onClick={closeNavbar}>Back Office Services</Link></li>
+                <li><Link className="dropdown-item" to="/data-processing" onClick={closeNavbar}>Data Processing</Link></li>
               </ul>
             </li>
 
-            <li className="nav-item"><Link className="nav-link" to="/contact">Contact Us</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/contact" onClick={closeNavbar}>Contact Us</Link></li>
           </ul>
         </div>
       </div>
